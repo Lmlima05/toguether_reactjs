f4e5d15d9ff755966e277e61641e94397b1f943e
Luan Melo <luanmelolima@gmail.com>
2024-10-14 10:03:22 -0300

    Ajuste estilização do room e verificação de login

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -42,6 +42,8 @@ export function Room() {
     };
 
     await database.ref(`rooms/${roomId}/question`).push(question);
+
+    setNewQuestion('');
   }
 
   return (
@@ -67,7 +69,14 @@ export function Room() {
           />
 
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            { user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            ) }
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
