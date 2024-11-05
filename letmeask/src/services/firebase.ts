d316bf2de8e98b815c44034dca9bc50117784b07
Luan Melo <luanmelolima@gmail.com>
2024-11-05 07:44:48 -0300

    Realizando a integração com fire base

---
+++ letmeask/src/services/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzJW2hor5T9HVvh1LTTxwlylBDNGw-U0k",
+  authDomain: "letmeask-8e486.firebaseapp.com",
+  databaseURL: "https://letmeask-8e486-default-rtdb.firebaseio.com",
+  projectId: "letmeask-8e486",
+  storageBucket: "letmeask-8e486.firebasestorage.app",
+  messagingSenderId: "717919311496",
+  appId: "1:717919311496:web:d81aa8392dc153cfc0313d"
+};
+
+firebase.initializeApp(firebaseConfig);
