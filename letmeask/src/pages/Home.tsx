923c95bd614837e7000bb0e4101ec8853abeb322
Luan Melo <luanmelolima@gmail.com>
2024-11-12 22:03:42 -0300

    Criando o auth.css

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import illustrationImg from '../assets/images/illustration.svg'
+import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.css';
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
