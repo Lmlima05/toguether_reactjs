97522c635578a4869daf76e036a374faadc9b836
Luan Melo <luanmelolima@gmail.com>
2024-11-27 23:19:14 -0300

    trazendo informaçoes do google account

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -4,12 +4,33 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { auth, firebase } from './services/firebase';
 
 export const TestContext = createContext({} as any);
 
 function App() {
   const [value, setValue] = useState('Teste');
 
+  function signInWithGoole() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from google Account.')
+      } 
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      })
+      }
+    })
+  }
+ 
   return (  
     <BrowserRouter>
     <TestContext.Provider value={{ value, setValue }}>
