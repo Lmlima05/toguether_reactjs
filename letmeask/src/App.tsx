baa5f2303e1b82ccfdc4074e9ef1908c904d898a
Luan Melo <luanmelolima@gmail.com>
2024-12-06 11:05:17 -0300

    criando hook de autenticação

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
-import { AuthContextProvider } from './contexs/AuthContex';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (  
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,7 +1,4 @@
-import { useContext} from 'react';
 import { useNavigate } from 'react-router-dom'
-
-import { AuthContext } from '../contexs/AuthContex'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,11 +7,14 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.css';
+import { useAuth } from '../hooks/useAuth';
 
 
 export function Home() {
   const history = useNavigate();
-  const { user, signInWithGoogle } = useContext(AuthContext)
+  const { user, signInWithGoogle } = useAuth();
+
+
 
   async function handleCreateRoom() {
     if (!user) {
